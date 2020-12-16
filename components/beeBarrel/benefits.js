874ad5a25cf8a9ec982f2111e0be31c5d64e522f
Saia Fonua <saiafonua@users-MacBook-Pro.local>
2020-12-15 21:00:14 -0700

    responsive images with picture tag

--- components/beeBarrel/benefits.js
+++ components/beeBarrel/benefits.js
@@ -13,13 +13,23 @@ const Benefits = ({ title, subtitle, text, text2, images, imagesText }) => {
       {images ? (
         <div className="flex space-x-4 pb-8">
           <div>
-            <img className="pb-2" src={`${images[0]}`} />
+          <picture>
+            <source media="(max-width: 640px)" srcset="images/beeBarrel/bees-drawing-comb-174w.jpg" />
+            <source media="(max-width: 1024px)" srcset="images/beeBarrel/bees-drawing-comb-257w.jpg" />
+            <source media="(min-width: 1024px)" srcset="images/beeBarrel/bees-drawing-comb-441w.png" />
+            <img src="images/beeBarrel/bees-drawing-comb-174w.jpg" alt="Chris standing up holding his daughter Elva" />
+          </picture>
             <h5 className="text-xs" style={{ maxWidth: `10rem` }}>
               {imagesText[0]}
             </h5>
           </div>
           <div>
-            <img className="pb-2" src={`${images[1]}`} />
+          <picture>
+            <source media="(max-width: 640px)" srcset="images/beeBarrel/hive-unique-161w.jpg" />
+            <source media="(max-width: 1024px)" srcset="images/beeBarrel/hive-unique-258w.png" />
+            <source media="(min-width: 1024px)" srcset="images/beeBarrel/hive-unique-441w.png" />
+            <img src="images/beeBarrel/hive-unique-161w.jpg" alt="Chris standing up holding his daughter Elva" />
+          </picture>
             <h5 className="text-xs" style={{ maxWidth: `10rem` }}>
               {imagesText[1]}
             </h5>
