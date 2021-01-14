af179077afefb6c05ba7dedc3af1a6ef8ce386a3
Saia Fonua <saiafonua@users-MacBook-Pro.local>
2021-01-14 14:51:00 -0700

    Invest link

--- components/header.js
+++ components/header.js
@@ -15,23 +15,25 @@ export default function Header() {
         </button>
 
         <nav className="ml-5">
-          <a>
-            <picture>
-              <source
-                media="(max-width: 767px)"
-                srcSet="/images/icon-small.svg"
-              />
-              <source
-                media="(max-width: 1024px)"
-                srcSet="/images/icon-medium.svg"
-              />
-              <source
-                media="(min-width: 1024px)"
-                srcSet="/images/icon-large.svg"
-              />
-              <img src="/images/logo.svg" className="stroke-1" />
-            </picture>
-          </a>
+          <Link href="/">
+            <a>
+              <picture>
+                <source
+                  media="(max-width: 767px)"
+                  srcSet="/images/icon-small.svg"
+                />
+                <source
+                  media="(max-width: 1024px)"
+                  srcSet="/images/icon-medium.svg"
+                />
+                <source
+                  media="(min-width: 1024px)"
+                  srcSet="/images/icon-large.svg"
+                />
+                <img src="/images/logo.svg" className="stroke-1" />
+              </picture>
+            </a>
+          </Link>
           <nav
             className={`fixed top-0 w-full bg-white left-0 h-full overflow-x-hidden overflow-y-scroll p-4 z-20 ${
               !isOpen ? 'hidden' : ''
@@ -50,8 +52,8 @@ export default function Header() {
               <Link href="/about">
                 <a className="mb-4 leading-8 block text-sm">ABOUT US</a>
               </Link>
-              <Link href="/contribute">
-                <a className="mb-4 leading-8 block text-sm">CONTRIBUTE</a>
+              <Link href="/investor">
+                <a className="mb-4 leading-8 block text-sm">INVEST</a>
               </Link>
             </div>
           </nav>
@@ -59,7 +61,7 @@ export default function Header() {
       </div>
       <nav className="self-end">
         <ul className="hidden md:flex flex-row space-x-8 md:space-x-14 mr-4 md: mr-6 text-darkish-blue">
-          <li>
+          {/* <li>
             <Link href="/">
               <a
                 className={`${
@@ -69,12 +71,14 @@ export default function Header() {
                 HOME
               </a>
             </Link>
-          </li>
+          </li> */}
           <li>
             <Link href="/beeBarrel">
               <a
                 className={`${
-                  useRouter().pathname === '/beeBarrel' ? 'font-black text-sm lg:text-lg' : 'text-sm lg:text-lg'
+                  useRouter().pathname === '/beeBarrel'
+                    ? 'font-black text-sm lg:text-lg'
+                    : 'text-sm lg:text-lg'
                 }`}
               >
                 BEE BARREL
@@ -85,18 +89,28 @@ export default function Header() {
             <Link href="/about">
               <a
                 className={`${
-                  useRouter().pathname === '/about' ? 'font-black text-sm lg:text-lg' : 'text-sm lg:text-lg'
+                  useRouter().pathname === '/about'
+                    ? 'font-black text-sm lg:text-lg'
+                    : 'text-sm lg:text-lg'
                 }`}
               >
                 ABOUT US
               </a>
             </Link>
           </li>
-          {/* <li>
-            <Link href="/contribute">
-              <a>CONTRIBUTE</a>
+          <li>
+            <Link href="/investor">
+              <a
+                className={`${
+                  useRouter().pathname === '/investor'
+                    ? 'font-black text-sm lg:text-lg'
+                    : 'text-sm lg:text-lg'
+                }`}
+              >
+                INVEST
+              </a>
             </Link>
-          </li> */}
+          </li>
         </ul>
       </nav>
     </header>
